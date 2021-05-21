c9783f9c80c2f8a62f93d4b0454234c089b278e3
noelib230802 <82592862+noelib230802@users.noreply.github.com>
2021-05-21 22:11:51 +0200

    Create creaProcUS09.sql

---
+++ backend/procs/creaProcUS09.sql
@@ -0,0 +1,8 @@
+CREATE PROCEDURE "DBA"."US09"(in idPers INTEGER, in codePostal INTEGER, in visite DATE)
+
+BEGIN
+	INSERT INTO tbVisites
+    (persId, codePost, dateVis)
+    VALUES 
+    (idPers, codePostal, visite)
+END
