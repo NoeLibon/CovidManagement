df21ba4bae6fa9376c3841089074f48d93fbea5d
noelib230802 <82592862+noelib230802@users.noreply.github.com>
2021-05-21 22:12:11 +0200

    Update creaProcUS09.sql

--- backend/procs/creaProcUS09.sql
+++ backend/procs/creaProcUS09.sql
@@ -2,7 +2,7 @@ CREATE PROCEDURE "DBA"."US09"(in idPers INTEGER, in codePostal INTEGER, in visit
 
 BEGIN
 	INSERT INTO tbVisites
-    (persId, codePost, dateVis)
-    VALUES 
-    (idPers, codePostal, visite)
+    	(persId, codePost, dateVis)
+    	VALUES 
+    	(idPers, codePostal, visite)
 END
