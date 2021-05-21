dcd801c1bdca06884e26d7f6c8a1a1d616665172
noelib230802 <82592862+noelib230802@users.noreply.github.com>
2021-05-21 22:06:50 +0200

    Update creaProcUS05.sql

--- backend/procs/creaProcUS05.sql
+++ backend/procs/creaProcUS05.sql
@@ -1,4 +1,4 @@
-ALTER PROCEDURE "DBA"."US05"(in test DATE, in exp DATE, in resul VARCHAR(20), in idPers INTEGER)
+CREATE PROCEDURE "DBA"."US05"(in test DATE, in exp DATE, in resul VARCHAR(20), in idPers INTEGER)
 BEGIN
     INSERT INTO tbTests
     (dateTest, expiration, resultat, persId)
