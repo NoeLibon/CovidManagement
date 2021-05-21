b6eaba67b27e86136f67af258b16744f5a5f5d6b
noelib230802 <82592862+noelib230802@users.noreply.github.com>
2021-05-21 22:11:08 +0200

    Update creaProcUS08.sql

--- backend/procs/creaProcUS08.sql
+++ backend/procs/creaProcUS08.sql
@@ -2,13 +2,13 @@ CREATE PROCEDURE "DBA"."US08"()
 
 BEGIN
 	  SELECT code, lib, count(resultat) AS nCasCovid FROM tbTests
-    JOIN tbPers
-    ON tbTests.persId = tbPers.persId
-    JOIN tbVisites
-    ON tbPers.persId = tbVisites.persId
-    JOIN tbVilles
-    ON tbVisites.codePost = tbVilles.code
-    WHERE resultat = 'positif' AND expiration >= getdate() AND dateVis < getdate()
-    GROUP BY code, lib
-    HAVING nCasCovid > 1
+	    JOIN tbPers
+	    ON tbTests.persId = tbPers.persId
+	    JOIN tbVisites
+	    ON tbPers.persId = tbVisites.persId
+	    JOIN tbVilles
+	    ON tbVisites.codePost = tbVilles.code
+	    WHERE resultat = 'positif' AND expiration >= getdate() AND dateVis < getdate()
+	    GROUP BY code, lib
+	    HAVING nCasCovid > 1
 END
