420e3806ba67aff276d06df8d1e2a0970fc6c74a
noelib230802 <82592862+noelib230802@users.noreply.github.com>
2021-05-21 22:00:15 +0200

    Update creaProcUS03.sql

--- backend/procs/creaProcUS03.sql
+++ backend/procs/creaProcUS03.sql
@@ -2,5 +2,5 @@ CREATE PROCEDURE "DBA"."US03"()
 
 BEGIN
 	SELECT tbPers.persId, prenom, nom, naiss, dateTest, expiration, resultat FROM tbPers
-    NATURAL FULL JOIN tbTests
+    	NATURAL FULL JOIN tbTests
 END
